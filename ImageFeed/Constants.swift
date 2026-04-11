00d7c2df650a55edd13fe378d98b36974c52abc5
bacik4 <igorekgk2016@gmail.com>
2026-04-11 18:31:03 +0300

    no message

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//
+
+enum Constants {
+    static let accessKey = "<_lyUdHbqAR7qqTdAyspIkQvOeKcl99PaH4cf3qKNL6g>"
+    static let secretKey = "<pA3Vcr05hIRgUTMZKnSHKQNw9ON9YCPyBQcr8WZ5zCI>"
+    static let redirectURI = "<urn:ietf:wg:oauth:2.0:oob>"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURLString = "https://api.unsplash.com"
+}
+
+//redirectURIc
---
+++ ImageFeed/O2Auth/OAuth2Service.swift
@@ -0,0 +1,81 @@
+//
+//  OAuth2Service.swift
+//  ImageFeed
+//
+//
+import Foundation
+
+final class OAuth2Service {
+    static let shared = OAuth2Service()
+    private init() {}
+    
+    private let tokenStorage = OAuth2TokenStorage()
+    
+    private func makeOAuthTokenRequest(code: String) -> URLRequest? {
+        guard var urlComponents = URLComponents(string: "https://unsplash.com/oauth/token") else{
+            return nil
+        }
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "client_secret", value: Constants.secretKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "code", value: code),
+            URLQueryItem(name: "grant_type", value: "authorization_code"),
+        ]
+        guard let authTokenUrl = urlComponents.url else {
+            return nil
+        }
+        var request = URLRequest(url: authTokenUrl)
+        request.httpMethod = "POST"
+        return request
+    }
+    
+    func fetchOAuthToken(
+        code: String,
+        completion: @escaping (Result<String,Error>) -> Void){
+            guard let request = makeOAuthTokenRequest(code: code) else {
+                completion(.failure(NetworkError.invalidRequest))
+                return
+            }
+            let task = URLSession.shared.data(for: request) { result in
+                switch result {
+                case .success(let (data, response)):
+                    guard 200..<300 ~= response.statusCode else {
+                        let errorBody = String(data: data, encoding: .utf8) ?? "Не удалось прочитать тело ответа"
+                            print("Ошибка Unsplash. Код: \(response.statusCode)")
+                            print("Тело ответа: \(errorBody)")
+                        completion(.failure(NetworkError.httpStatusCode(response.statusCode)))
+                        return
+                    }
+                    
+                    do {
+                        let responseBody = try JSONDecoder().decode(OAuthTokenResponseBody.self, from: data)
+                        self.tokenStorage.token = responseBody.accessToken
+                        completion(.success(responseBody.accessToken))
+                    } catch {
+                        print("Ошибка декодирования OAuthTokenResponseBody: \(error)")
+                        completion(.failure(NetworkError.decodingError(error)))
+                    }
+                    
+                case .failure(let error):
+                    print("Сетевая ошибка: \(error)")
+                    completion(.failure(error))
+                }
+            }
+            task.resume()
+        }
+}
+
+struct OAuthTokenResponseBody: Decodable {
+    let accessToken: String
+    let tokenType: String
+    let scope: String
+    let createdAt: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+        case tokenType = "token_type"
+        case scope
+        case createdAt = "created_at"
+    }
+}
